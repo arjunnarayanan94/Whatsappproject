b9bee468a14ec422c500dc5e816f7ebf2829a1c7
anandu2019 <nair.anandu94@gmail.com>
2021-10-20 16:52:01 +0530

    first commit

--- default.js
+++ default.js
@@ -47,7 +47,7 @@ function user4handler(options, event, context, callback) {
 function user4_1handler(options, event, context, callback) {
   let amount;
   if ((amount = /^\d+(\.\d{1,2})?$/.test(event.message))) {
-    options.next_state = "bot5_1";
+    options.next_state = "bot10";
   } else {
     options.next_state = "bot4";
   }
@@ -56,17 +56,44 @@ function user4_1handler(options, event, context, callback) {
 }
 
 function user4_2handler(options, event, context, callback) {
-  options.next_state = "bot5_2";
 
+  if(event.message==1){
+
+  options.next_state = "bot10_1";
+
+  }
   callback(options, event, context);
+
 }
+
+
+function userdetailhandler1(options, event, context, callback) {
+
+  if(event.message==1|| event.message==2){
+
+  //  options.next_state="bot6";
+  }
+
+else{
+   options.next_state="bot10_1";
+}
+  callback(options, event, context);
+
+}
+
+
 
 function user4_3handler(options, event, context, callback) {
-  options.next_state = "bot5_3";
+
+  if(event.message==2){
+  options.next_state = "bot10_2";
+  }
 
   callback(options, event, context);
 }
-function user4_4handler(options, event, context, callback) {
+
+function userdetailhandler2(options, event, context, callback) {
+
   let url;
   if (
     (url =
@@ -74,32 +101,86 @@ function user4_4handler(options, event, context, callback) {
         event.message
       ))
   ) {
-    options.next_state = "bot5_4";
+   
     console.log("Experience");
   } else {
-    options.next_state = "bot5_3";
+    options.next_state = "bot10_2";
   }
 
   callback(options, event, context);
 }
+
+
+
+
+
+function user4_4handler(options, event, context, callback) {
+  
+  if(event.message==3){
+
+    options.next_state = "bot10_3";
+    }
+  
+    callback(options, event, context);
+  }
+
+
+
+  function userdetailhandler3(options, event, context, callback) {
+    let phone1;
+    if ((phone1 = /^(\+\d{1,3}[- ]?)?\d{10}$/.test(event.message))) {
+      
+    } else {
+      options.next_state = "bot10_3";
+    }
+    callback(options, event, context);
+  }
+
+    
+
+
 
 function user4_5handler(options, event, context, callback) {
-  let phone1;
-  if ((phone1 = /^(\+\d{1,3}[- ]?)?\d{10}$/.test(event.message))) {
-    options.next_state = "bot5_5";
-  } else {
-    options.next_state = "bot5_4";
-  }
+  if(event.message==4){
+
+    options.next_state = "bot10_4";
+    }
+  
   callback(options, event, context);
 }
+
+function userdetailhandler4(options, event, context, callback) {
+ 
+
+  callback(options, event, context);
+}
+
+
 function user4_6handler(options, event, context, callback) {
-//   options.next_state = "bot6";
-  console.log("testing", event.message);
-  console.log("mini");
+  if(event.message==5){
+
+    options.next_state = "bot10_5";
+    }
+  
+  callback(options, event, context);
+}
+
+function userdetailhandler5(options, event, context, callback) {
+ 
 
   callback(options, event, context);
-
 }
+
+
+
+// function user4_7handler(options, event, context, callback) {
+// //   options.next_state = "bot6";
+//   console.log("testing", event.message);
+//   console.log("mini");
+
+//   callback(options, event, context);
+
+// }
 function user6_1handler(options, event, context, callback) {
   options.next_state = "bot7";
 
@@ -122,7 +203,7 @@ function user8_1handler(options, event, context, callback) {
   callback(options, event, context);
 }
 function user9_1handler(options, event, context, callback) {
-  options.next_state = "bot10";
+  options.next_state = "bot20";
 
   callback(options, event, context);
 }
@@ -133,11 +214,16 @@ module.exports.main = {
   user3: user3handler,
   user4: user4handler,
   user4_1: user4_1handler,
-  user4_2: user4_2handler,
-  user4_3: user4_3handler,
-  user4_4: user4_4handler,
-  user4_5: user4_5handler,
-  user4_6: user4_6handler
+  user11: user4_2handler,
+  user11a:userdetailhandler1,
+  user11_1: user4_3handler,
+  user11b:userdetailhandler2,
+  user11_2: user4_4handler,
+  user11c:userdetailhandler3,
+  user11_3: user4_5handler,
+  user11d:userdetailhandler4,
+  user11_4: user4_6handler,
+  user11e: userdetailhandler5
 };
 
 module.exports.nextoption = {
