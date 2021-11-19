d2fc98097866e687b7a60c1b48a665ba2bb86114
arjunnarayanan94 <arjunnarayanan94@gmail.com>
2021-11-19 17:35:06 +0530

    Set feedback to null

--- api.js
+++ api.js
@@ -1,91 +1,88 @@
 var axios = require("axios");
-let store = async (
-  UserType,
-  sender,
-  mobileNo,
-  bankRef,
-  transactionDate,
-  transactionAmount,
-  fraudType,
-  fraudMethod,
-  description,
-  comment,
-  attachmentUrl
+let store = async(
+    UserType,
+    sender,
+    mobileNo,
+    bankRef,
+    transactionDate,
+    transactionAmount,
+    fraudType,
+    fraudMethod,
+    description,
+    comment,
+    attachmentUrl
 ) => {
-  var data = JSON.stringify({
-    feedback: "Need_improvement",
+    var data = JSON.stringify({
+        feedback: "",
 
-    comment: comment,
+        comment: comment,
 
-    source: "whatsapp",
+        source: "whatsapp",
 
-    inputType: "TEXT",
+        inputType: "TEXT",
 
-    userType: UserType,
+        userType: UserType,
 
-    mobileNo: mobileNo,
+        mobileNo: mobileNo,
 
-    bankRef: bankRef,
+        bankRef: bankRef,
 
-    transactionDate: transactionDate,
+        transactionDate: transactionDate,
 
-    transactionAmount: transactionAmount,
+        transactionAmount: transactionAmount,
 
-    fraudType: fraudType,
+        fraudType: fraudType,
 
-    fraudMethod: fraudMethod,
+        fraudMethod: fraudMethod,
 
-    description: description,
+        description: description,
 
-    attachmentUrl: attachmentUrl,
+        attachmentUrl: attachmentUrl,
 
-    userToken: sender,
-  });
-  console.log("Testsee ", data);
+        userToken: sender,
+    });
+    console.log("Testsee ", data);
 
-  var config = {
-    method: "post",
-    url: "http://172.105.49.9:8090/cybersena/savetodashboard/en/",
-    headers: {
-      Connection: "keep-alive",
-      "sec-ch-ua":
-        '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
-      Accept: "application/json, text/plain, /",
-      localtimezone: "Asia/Calcutta",
-      "sec-ch-ua-mobile": "?0",
-      "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
-      "Sec-Fetch-Site": "same-origin",
-      "Sec-Fetch-Mode": "cors",
-      "Sec-Fetch-Dest": "empty",
-      appId: "9dc9030f-4276-4efc-b602-a46742e87a34",
-      "Content-Type": "application/json",
-    },
-    data: data,
-  };
+    var config = {
+        method: "post",
+        url: "http://172.105.49.9:8090/cybersena/savetodashboard/en/",
+        headers: {
+            Connection: "keep-alive",
+            "sec-ch-ua": '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+            Accept: "application/json, text/plain, /",
+            localtimezone: "Asia/Calcutta",
+            "sec-ch-ua-mobile": "?0",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Dest": "empty",
+            appId: "9dc9030f-4276-4efc-b602-a46742e87a34",
+            "Content-Type": "application/json",
+        },
+        data: data,
+    };
 
-  return new Promise((resolve, reject) => {
-    axios(config)
-      .then(function (response) {
-        console.log("Hello ", JSON.stringify(response.data));
-        console.log('test12', data )
+    return new Promise((resolve, reject) => {
+        axios(config)
+            .then(function(response) {
+                console.log("Hello ", JSON.stringify(response.data));
+                console.log('test12', data)
 
-        try {
-          let a = JSON.stringify(response.data);
-          const ref = JSON.parse(a);
-          const result = ref.referenceId;
+                try {
+                    let a = JSON.stringify(response.data);
+                    const ref = JSON.parse(a);
+                    const result = ref.referenceId;
 
-          resolve(result);
-        } catch (err) {
-          resolve();
-        }
-      })
-      .catch(function (error) {
-        reject(err);
-        console.log(error);
-      });
-  });
+                    resolve(result);
+                } catch (err) {
+                    resolve();
+                }
+            })
+            .catch(function(error) {
+                reject(err);
+                console.log(error);
+            });
+    });
 };
 
 exports.store = store;
-
